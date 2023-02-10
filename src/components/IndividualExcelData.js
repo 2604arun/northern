61d702ad9a692343456f83d9741100c7fa9ca550
2604arun <125002654+2604arun@users.noreply.github.com>
2023-02-10 13:49:39 +0530

    Add files via upload

---
+++ src/components/IndividualExcelData.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const IndividualData = (props) => {
+  const {individualExcelData, handleEditClick, handleDelete}= props
+  return (
+      <>
+          <td>{individualExcelData.Id}</td>
+          <td>{individualExcelData.FirstName}</td>
+          <td>{individualExcelData.LastName}</td>
+          <td>{individualExcelData.gender}</td>
+          <td>{individualExcelData.country}</td>
+          <td>{individualExcelData.age}</td>
+          <td>
+            <button onClick={(e)=>handleEditClick(e,individualExcelData)}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
+          </td>
+      </>
+  )
+}
+
+export default IndividualData
